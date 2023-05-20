a46d7c0bb11bfd8202b17f1af0970acf78d549d4
Jacek Pasierb <jack_byk@o2.pl>
2023-05-20 23:32:40 +0200

    first commit

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import css from './ContactFormStyle.module.css';
+
+export class ContactForm extends Component {
+  render() {
+    return (
+      <div className={css.contactForm}>
+        <label>Name</label>
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+        <button type="submit">Add contact</button>
+      </div>
+    );
+  }
+}
