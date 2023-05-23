9e3038e3b733097358ea4a862cfc404e432976fc
Jacek Pasierb <jack_byk@o2.pl>
2023-05-23 21:43:47 +0200

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,15 +6,8 @@ import { Filter } from './Filter/Filter';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
-    name: '',
-    number: '',
   };
   addContact = (id, name, number) => {
     // const { contacts } = this.state;
@@ -22,8 +15,6 @@ export class App extends Component {
     this.setState(prevState => ({
       contacts: [...prevState.contacts, newContact],
       filter: '',
-      name: '',
-      number: '',
     }));
   };
   changeName = e => this.setState({ name: e.target.value });
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,10 +1,19 @@
+import { nanoid } from 'nanoid';
 import React, { Component } from 'react';
+import css from './FilterStyle.module.css'
 
 export class Filter extends Component {
+  filterInputId = nanoid();
   render() {
     return (
-      <div>
-        <input type="text" onChange={this.props.changeFilter} />
+      <div className={css.filter}>
+        <label htmlFor={this.filterInputId}>Find contacts by name</label>
+        <input
+          type="text"
+          name="filter"
+          id={this.filterInputId}
+          onChange={this.props.changeFilter}
+        />
       </div>
     );
   }
